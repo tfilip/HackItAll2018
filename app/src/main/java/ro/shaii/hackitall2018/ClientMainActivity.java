bdcec3bc001d68c5995689805725b8266d147b7b
Filip Tepes-Onea <filip.tepes@outlook.com>
2018-03-24 16:24:35 +0200

    deautentificare

--- app/src/main/java/ro/shaii/hackitall2018/ClientMainActivity.java
+++ app/src/main/java/ro/shaii/hackitall2018/ClientMainActivity.java
@@ -1,8 +1,15 @@
 package ro.shaii.hackitall2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ClientMainActivity extends AppCompatActivity {
 
@@ -10,7 +17,32 @@ public class ClientMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Toast.makeText(this,"Client",Toast.LENGTH_SHORT).show();
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuLogOff:
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+
+        return true;
+    }
+
 }
--- app/src/main/java/ro/shaii/hackitall2018/RestaurantMain2Activity.java
+++ app/src/main/java/ro/shaii/hackitall2018/RestaurantMain2Activity.java
@@ -1,7 +1,14 @@
 package ro.shaii.hackitall2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RestaurantMain2Activity extends AppCompatActivity {
 
@@ -9,5 +16,30 @@ public class RestaurantMain2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_main2);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuLogOff:
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+
+        return true;
+    }
+
 }
