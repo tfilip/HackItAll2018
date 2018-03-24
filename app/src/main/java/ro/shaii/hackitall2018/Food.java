7ee08b73f6a07b77f85b0e3c02bb010b0ca23c53
Filip Tepes-Onea <filip.tepes@outlook.com>
2018-03-24 17:04:05 +0200

    added food

---
+++ app/src/main/java/ro/shaii/hackitall2018/Food.java
@@ -0,0 +1,76 @@
+package ro.shaii.hackitall2018;
+
+import java.util.Date;
+
+/**
+ * Created by filip on 24-Mar-18.
+ */
+
+public class Food {
+
+    public String foodName;
+    public restaurant rest;
+    public Date productionDate;
+    public Date expirtyDate;
+    public String descriere;
+
+    public Food(){
+
+    }
+
+    public Food(String foodName, restaurant restaurant, Date productionDate, Date expirtyDate,String descriere) {
+        this.foodName = foodName;
+        this.rest = restaurant;
+        this.productionDate = productionDate;
+        this.expirtyDate = expirtyDate;
+        this.descriere = descriere;
+    }
+
+    public restaurant getRest() {
+        return rest;
+    }
+
+    public void setRest(restaurant rest) {
+        this.rest = rest;
+    }
+
+    public String getDescriere() {
+        return descriere;
+    }
+
+    public void setDescriere(String descriere) {
+        this.descriere = descriere;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public restaurant getRestaurant() {
+        return this.rest;
+    }
+
+    public void setRestaurant(restaurant Restaurant) {
+        this.rest = Restaurant;
+    }
+
+    public Date getProductionDate() {
+        return productionDate;
+    }
+
+    public void setProductionDate(Date productionDate) {
+        this.productionDate = productionDate;
+    }
+
+    public Date getExpirtyDate() {
+        return expirtyDate;
+    }
+
+    public void setExpirtyDate(Date expirtyDate) {
+        this.expirtyDate = expirtyDate;
+    }
+}
